8d9c3f861d61c657379af17500fd73fcd5bc03d5
hithesh-k <hithesh.kumar@nineleaps.com>
2020-09-09 16:12:30 +0530

    [base] initial commit

---
+++ src/main/java/com/nineleaps/greytHRClone/GreytHrCloneApplication.java
@@ -0,0 +1,13 @@
+package com.nineleaps.greytHRClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreytHrCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreytHrCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nineleaps/greytHRClone/GreytHrCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nineleaps.greytHRClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreytHrCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
