3e1c7973c14feca2308325714514682b1e84c632
Shinazm <mohammad.shinaz@nineleaps.com>
2020-09-10 16:42:28 +0530

    [feature] added signup method

--- src/main/java/com/nineleaps/greytHRClone/controller/EmployeeAuthentication.java
+++ src/main/java/com/nineleaps/greytHRClone/controller/EmployeeAuthentication.java
@@ -1,4 +1,39 @@
 package com.nineleaps.greytHRClone.controller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.nineleaps.greytHRClone.model.EmployeeData;
+import com.nineleaps.greytHRClone.service.AuthenticationService;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
 public class EmployeeAuthentication {
+
+    @Autowired
+    AuthenticationService authenticationService;
+
+
+   @PostMapping(path="/employee")
+    public String Signup(@RequestBody EmployeeData employeeData){
+      return authenticationService.Signup(employeeData);
+   }
+
+
+
+
+
+
+
+   @GetMapping(path = "/profile")
+    public EmployeeData profile(@RequestParam(value = "id") int id){
+       return authenticationService.profile(id);
+   }
+
+
+
+
+
 }
--- src/main/java/com/nineleaps/greytHRClone/model/EmployeeData.java
+++ src/main/java/com/nineleaps/greytHRClone/model/EmployeeData.java
@@ -1,4 +1,32 @@
 package com.nineleaps.greytHRClone.model;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name="employee_data")
 public class EmployeeData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int empId ;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="email")
+    private String email;
+
+
 }
