bc814b65996eeabf88e29c0ca930db3526237fcc
Shinazm <mohammad.shinaz@nineleaps.com>
2020-09-11 17:40:56 +0530

    [profile]feature |addedmore attributes in employee

--- src/main/java/com/nineleaps/greytHRClone/model/EmployeeData.java
+++ src/main/java/com/nineleaps/greytHRClone/model/EmployeeData.java
@@ -1,10 +1,14 @@
 package com.nineleaps.greytHRClone.model;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.sql.Date;
+
 @Data
 @Entity
 @Getter
@@ -24,6 +28,41 @@ public class EmployeeData {
 
     @Column(name="email")
     private String email;
+
+    @Column(name="manager_id")
+    private int managerId;
+
+    @Column(name="dob")
+    @JsonFormat(pattern="dd-MM-yyyy")
+    private Date dob;
+
+    @Column(name="location")
+    private String location;
+
+    @Column(name="created_date")
+    private Date createdDate;
+
+    @Column(name="employee_department")
+    private String employeeDepartment;
+
+    @Column(name="employee_designation")
+    private String employeeDesignation;
+
+    public String getEmployeeDepartment() {
+        return employeeDepartment;
+    }
+
+    public void setEmployeeDepartment(String employeeDepartment) {
+        this.employeeDepartment = employeeDepartment;
+    }
+
+    public String getEmployeeDesignation() {
+        return employeeDesignation;
+    }
+
+    public void setEmployeeDesignation(String employeeDesignation) {
+        this.employeeDesignation = employeeDesignation;
+    }
 
     public int getEmpId() {
         return empId;
@@ -57,6 +96,40 @@ public class EmployeeData {
         this.email = email;
     }
 
+
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public int getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(int managerId) {
+        this.managerId = managerId;
+    }
+
     @Override
     public String toString() {
         return "EmployeeData{" +
@@ -64,6 +137,12 @@ public class EmployeeData {
                 ", name='" + name + '\'' +
                 ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
+                ", managerId='" + managerId + '\'' +
+                ", dob=" + dob +
+                ", location='" + location + '\'' +
+                ", createdDate=" + createdDate +
+                ", employeeDepartment='" + employeeDepartment + '\'' +
+                ", employeeDesignation='" + employeeDesignation + '\'' +
                 '}';
     }
 }
