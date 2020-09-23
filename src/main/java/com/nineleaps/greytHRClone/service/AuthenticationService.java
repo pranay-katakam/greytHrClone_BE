88fa75291d3008824764dadef3d2e070b86a7f2f
hithesh-k <hithesh.kumar@nineleaps.com>
2020-09-23 09:49:08 +0530

    [Authentication] fixes updated cookie to response cookie

--- src/main/java/com/nineleaps/greytHRClone/service/AuthenticationService.java
+++ src/main/java/com/nineleaps/greytHRClone/service/AuthenticationService.java
@@ -6,6 +6,7 @@ import com.nineleaps.greytHRClone.repository.EmployeeDataRepository;
 
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.WebUtils;
@@ -49,7 +50,7 @@ public class AuthenticationService {
                 String dbpassword = (String) dbuser.get("password");
                 int id = (int) dbuser.get("emp_id");
                 if (dbpassword.equals(password)) {
-                    generateCoookie(response, id);
+                    generateCookie(response, id);
                     JSONObject responseMsg = new JSONObject();
                     responseMsg.put("message", "Login Successful");
                     return ResponseEntity.status(OK).body(responseMsg);
@@ -67,29 +68,27 @@ public class AuthenticationService {
 
     }
 
-    private void generateCoookie(HttpServletResponse response, int id) {
+    private void generateCookie(HttpServletResponse response, int id) {
         String Id = String.valueOf(id);
-        Cookie cookie = new Cookie("userID", Id);
-        cookie.setSecure(false); // determines whether the cookie should only be sent using a secure protocol,
-        cookie.setMaxAge(5000); // A negative value means that the cookie is not stored persistently and will be //Session
-        cookie.setComment("");
-        cookie.setPath("/"); // The cookie is visible to all the pages in the directory you specify, and all
-        cookie.setHttpOnly(true);
-        response.addCookie(cookie);
+        ResponseCookie resCookie = ResponseCookie.from("userID", Id)
+                .httpOnly(true)
+                .sameSite("None")
+                .secure(false)
+                .path("/")
+                .maxAge(Math.toIntExact(7 * 24 * 60 * 60)) // expires in 7 days
+                .build();
+        response.addHeader("Set-Cookie", resCookie.toString());
+
     }
 
 
     public ResponseEntity<String> Logout(HttpServletRequest request, HttpServletResponse response) {
-        Cookie cookie = WebUtils.getCookie(request, "userID");
-        if (cookie != null) {
-            cookie.setMaxAge(0);
-            response.addCookie(cookie);
-            return ResponseEntity.status(OK).body("User Successfully Logged Out");
+        Cookie cookie = new Cookie("userID", null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
+        return ResponseEntity.status(OK).body("Successfully logged out");
 
-        } else {
-            return ResponseEntity.status(HttpServletResponse.SC_REQUEST_TIMEOUT).body("Session Expired");
-
-        }
     }
 }
+
 
