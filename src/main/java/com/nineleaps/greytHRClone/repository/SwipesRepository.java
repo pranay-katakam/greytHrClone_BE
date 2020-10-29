e05fc8641c6d3a46cc5d42951962ff16ed328841
hithesh-k <hithesh.kumar@nineleaps.com>
2020-10-29 13:04:14 +0530

    [Holidays] feature added holidays controller and services

---
+++ src/main/java/com/nineleaps/greytHRClone/repository/SwipesRepository.java
@@ -0,0 +1,7 @@
+package com.nineleaps.greytHRClone.repository;
+
+import com.nineleaps.greytHRClone.model.Swipes;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SwipesRepository extends CrudRepository<Swipes, Integer> {
+}
