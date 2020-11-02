9981892f29d6f7a1b0b168245534a72749269541
hithesh-k <hithesh.kumar@nineleaps.com>
2020-11-02 12:39:19 +0530

    [ Swipes ] feature added swipes DTO

--- src/main/java/com/nineleaps/greytHRClone/repository/SwipesRepository.java
+++ src/main/java/com/nineleaps/greytHRClone/repository/SwipesRepository.java
@@ -1,8 +1,15 @@
 package com.nineleaps.greytHRClone.repository;
 
+import com.nineleaps.greytHRClone.model.EmployeeData;
 import com.nineleaps.greytHRClone.model.Swipe;
+import org.json.simple.JSONObject;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface SwipesRepository extends JpaRepository<Swipe, Integer> {
+    @Query("select * from Swipe where empId=?1")
+    Iterable<Swipe> getSwipes(EmployeeData employeeData);
 }
